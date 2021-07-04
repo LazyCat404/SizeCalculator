a62f46c0645456c6a23c4d8183d1a58119393031
LazyCat404 <wenjie_address@outlook.com>
2021-07-04 16:49:29 +0800

    尺码静态页

--- src/components/index/SizeForm.vue
+++ src/components/index/SizeForm.vue
@@ -9,19 +9,48 @@
         <div class="title_form">
             <div>
                 <span>基本信息</span>
-                <span>
-                    如何测量
+                <span class="measure_btn">
+                    <van-icon name="question-o" />
+                    <span>如何测量</span>
                 </span>
             </div>
         </div>
         <div class="form_box">
-
+            <div>
+                <span class="form_lable">上胸围</span>
+                <van-field v-model="state.tBust" type="number" placeholder="请输入上胸围（cm）" />
+            </div>
+            <div>
+                <span class="form_lable">下胸围</span>
+                <van-field v-model="state.tBust" type="number" placeholder="请输入下胸围（cm）" />
+            </div>
+            <div>
+                <span class="form_lable">臀围</span>
+                <van-field v-model="state.tBust" type="number" placeholder="请输入臀围（cm）" />
+            </div>
+            <div>
+                <span class="form_lable">腿围</span>
+                <van-field v-model="state.tBust" type="number" placeholder="请输入腿围（cm）" />
+            </div>
+            <div>
+                <span class="form_lable">验证码</span>
+                <van-field v-model="state.tBust" type="number" placeholder="请输入验证码" />
+                <span class="verification_code">1234</span>
+            </div>
+            <van-button color="#ff5a60">普通按钮</van-button>
         </div>
     </div>
 </template>
 
 <script setup>
 import { api } from 'apis/base_api.js'
+import { reactive } from 'vue';
+const state = reactive({
+    tBust: null,    // 上胸围
+    bBust: null,    // 下胸围
+
+});
+
 </script>
 
 <style scoped>
@@ -45,10 +74,59 @@ import { api } from 'apis/base_api.js'
     .form_body{
         position:absolute;
         top: 230px;
+        padding: 0 35px;
     }
     .title_form{
         font-size: 30px;
         color: #fff;
+        margin-left: 30px;
     }
-    .form_box{}
+    .measure_btn{
+        font-size:22px;
+        margin-left:23px;
+    }
+    .measure_btn span{
+        border-bottom: 1px solid #fff;
+        cursor: pointer;
+        margin-left:5px;
+    }
+    .form_box{
+        width: 620px;
+        margin-top: 22px;
+        padding: 60px 30px;
+        background: #fff;
+        border-radius: 35px; 
+        box-shadow:0px 1px 15px 2.5px #2e2d2f61;   
+        margin-bottom: 20px;
+    }
+    .form_box>div{
+        margin-bottom: 45px;
+        position: relative;
+    }
+    .form_lable{
+        font-size: 25px;
+        color: #606060;
+    }
+    .van-field{
+        padding: 0 5px;
+        border-bottom:1px solid #999;
+        font-size: 20px;
+    }
+    .van-button{
+        width: 597px;
+        height: 82px;
+        font-size: 28px;
+        margin: 0 auto;
+        display: block;
+        margin-top:70px;
+        border-radius: 10px;
+    }
+    .verification_code{
+        position: absolute;
+        letter-spacing:2px;
+        bottom: 5px;
+        right: 5px;
+        font-size: 24px;
+        color: #5f5f5f;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import i18n from "./plugins/i18n";
 import router from "./router";
 import store from "./store";
 
+import { 
+    Icon,
+    Field, 
+    Button
+} from 'vant';
+
 import 'amfe-flexible/index.js';
 import '@vant/touch-emulator';
 
@@ -12,6 +18,9 @@ import './assets/css/border.css';
 import './assets/css/base.css';
 
 createApp(App)
+    .use(Icon)
+    .use(Field)
+    .use(Button)
     .use(i18n)
     .use(store)
     .use(router)
